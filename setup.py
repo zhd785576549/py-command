83e5dbd07180fdbeb9e2e4218e87f4519f668498
张冬 <zhangd@lhcis.com>
2020-04-01 22:42:31 +0800

    [modify]Modify setup info for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import find_packages, setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
-    name="py_command",
+    name="py-command",
     author="SnoopyBoy",
     author_email="785576549@qq.com",
     packages=find_packages(),
     version="1.0.0",
     description="A small tool for python command and sub command.",
     license='MIT',
+    long_description=long_description,
+    url="https://github.com/zhd785576549/py-command",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
